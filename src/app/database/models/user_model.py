6513d88cb7d8996b47d755e019808e543c19a650
Felix Scheffler <felix.scheffler@singular-it.de>
2023-02-24 13:33:26 +0100

    Fixed user DTOs

--- src/app/database/models/user_model.py
+++ src/app/database/models/user_model.py
@@ -8,13 +8,13 @@ from src.app.database.setup_db import Base, dto_factory
 
 class User(BaseModel):
     __tablename__ = "users"
-    id: Mapped[int] = Column(Integer, primary_key=True)
+
     name: Mapped[str] = Column(String, unique=True)
     email: Mapped[str] = Column(String, unique=True)
     password: Mapped[str] = Column(String)
 
 
 UserCreateDTO = dto_factory("UserCreateDTO", User, exclude=["id", "created_at", "updated_at"])
-UserLoginDTO = dto_factory("UserLoginDTO", User, exclude=["id", "name"])
+UserLoginDTO = dto_factory("UserLoginDTO", User, exclude=["id", "name", "created_at", "updated_at"])
 UserGetDTO = dto_factory("UserGetDTO", User, exclude=["password"])
-UserUpdateDTO = Partial[dto_factory("UserUpdateDTO", User, exclude=["id", "password"])]
+UserUpdateDTO = Partial[dto_factory("UserUpdateDTO", User, exclude=["id", "password", "created_at", "updated_at"])]
